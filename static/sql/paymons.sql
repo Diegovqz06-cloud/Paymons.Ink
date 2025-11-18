2b835abc2db1e7aa75edca837c178f088209c946
eliasbecerra1030 <elias.becerra1030@alumnos.udg.mx>
2025-11-18 15:43:22 -0600

    sql

--- static/sql/paymons.sql
+++ static/sql/paymons.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-08-2025 a las 23:38:12
+-- Tiempo de generación: 18-11-2025 a las 21:41:05
 -- Versión del servidor: 8.0.43
 -- Versión de PHP: 7.4.9
 
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `mensajes de contacto`
+--
+
+CREATE TABLE `mensajes de contacto` (
+  `id` int NOT NULL,
+  `nombre` text NOT NULL,
+  `correo` text NOT NULL,
+  `idea_tatuaje` text NOT NULL,
+  `usuario_id` int NOT NULL,
+  `celular` text NOT NULL,
+  `inspiracion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL,
+  `nombre` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -40,11 +67,25 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `correo`, `clave`, `perfil`) VALUES
-(1, 'Diego Vazquez', 'diego.vazquez2747@alumnos.udg.mx', 'scrypt:32768:8:1$DoACJjbBv1fyHnuW$76bfd3a1fd788293d48ad05525ec03c0fa53e5884530587106f58d82c2e3dae4d67607047397e7ebbd55e3084b6113f9c882911a99e406f5176cf23cd5459d66', 'U');
+(2, 'ADMIN', 'admin@gmail.com', 'scrypt:32768:8:1$XBajS7CdM84Lm5DR$d0284b8e9d312ab45f44f47c57f980b050b521afd39983d78c8f1b4bddfc0028881a2ad109fac311e94eaf66bda6360731ae92992be6b2c2965aa23d75f31730', 'A'),
+(5, 'Adan', 'adan@gmail.com', 'scrypt:32768:8:1$8pj0ITeTQdhyDu52$ca8cfde5b350de6c1c53cb9b99df6d64c1e18c487a7a63c7546f6a758c19e91be5113327a5b9dd8e1c1a3b7841553c319dea20fa2b1daa6ff042bc5f888c2c23', 'U');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `mensajes de contacto`
+--
+ALTER TABLE `mensajes de contacto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuario`
@@ -58,10 +99,32 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `mensajes de contacto`
+--
+ALTER TABLE `mensajes de contacto`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mensajes de contacto`
+--
+ALTER TABLE `mensajes de contacto`
+  ADD CONSTRAINT `mensajes de contacto_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
